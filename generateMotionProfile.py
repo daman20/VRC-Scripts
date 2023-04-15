aa7946a3ddcb60df983b8cbaf932e7a1bd6c2c54
daman20 <79991838+daman20@users.noreply.github.com>
2023-04-15 09:51:31 -0400

    Create generateMotionProfile.py

---
+++ generateMotionProfile.py
@@ -0,0 +1,82 @@
+import tkinter
+import cv2
+
+#   REQUIRED: tkinter, cv2, and a field image named "vex field.png"
+#   The field image must be in the same directory as this file, and must be an image of ONLY the vex field
+#   The field can have items on it (like game objects), but cannot have anything outside of the field walls
+
+
+
+
+
+#   This program is used to generate a motion profile for the robot
+#   The user clicks on the field to place points
+#   The program then prints the points in the format that the robot can use
+#   The format is used for OKAPILIB motion profiles, see 
+#   https://okapilib.github.io/OkapiLib/md_docs_tutorials_concepts_twodmotionprofiling.html
+#   for more information
+
+# init tk
+root = tkinter.Tk()
+
+
+#get the width and height of the image
+img = cv2.imread("vex field.png")
+CANVAS_HEIGHT, CANVAS_WIDTH, no_channels = img.shape
+
+#constants
+FIELD_WIDTH = 24
+FIELD_HEIGHT = 24
+FEETTOPIXELS = FIELD_WIDTH/CANVAS_WIDTH
+CIRCLE_DIAMETER = 30
+
+
+# create canvas
+myCanvas = tkinter.Canvas(root, bg="white", height=CANVAS_HEIGHT, width=CANVAS_WIDTH)
+
+# draw image
+field = tkinter.PhotoImage(file = "vex field.png")
+myCanvas.create_image(0, 0, image = field, anchor = tkinter.NW)
+
+
+points = []
+
+
+def drawLine(p1, p2):
+    myCanvas.create_line(p1.x_pix, p1.y_pix, p2.x_pix, p2.y_pix, fill="red")
+
+
+class point:
+    def __init__(self, x, y, angle):
+        self.x = x
+        self.y = y
+        self.ang = angle
+        self.x_pix = x * (1/FEETTOPIXELS)
+        self.y_pix = y * (1/FEETTOPIXELS)
+        myCanvas.create_oval(self.x_pix - CIRCLE_DIAMETER, self.y_pix - CIRCLE_DIAMETER, self.x_pix + CIRCLE_DIAMETER, self.y_pix + CIRCLE_DIAMETER, fill="red")
+        #draw a line between the last two points
+        if len(points) > 1:
+            drawLine(points[-2], points[-1])
+
+    def __str__(self):
+        return f"{{{round(self.x)}_ft, {round(self.y)}_ft, {round(self.ang)}_deg}}"
+
+
+def placePoint(e):
+    points.append(point(e.x * FEETTOPIXELS, e.y * FEETTOPIXELS, 0))
+
+
+# bind mouse click to placePoint
+myCanvas.bind("<Button-1>", placePoint)
+
+
+# add to window and show
+myCanvas.pack()
+root.mainloop()
+
+
+# print the points
+for i in points:
+    print(i)
+    if(i != points[-1]):
+        print(",")
